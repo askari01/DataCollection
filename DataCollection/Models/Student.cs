ff7fc39ab6d2917a4b3449b61dd91903f26c066d
Farrukh Askari <farrukh.askari01@gmail.com>
2015-03-01 14:40:57 +0500

    Data Collection
    Basic Form Insert, Update and Delete

---
+++ DataCollection/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DataCollection.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string FName { get; set; }
+        public string LName { get; set; }
+
+        public int MyProperty { get; set; }
+    }
+}
